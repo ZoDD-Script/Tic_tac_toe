76bb40b30f3df52084893199f00b7c826f40c3dd
Chukwu Blessed <blessedchukwu89@gmail.com>
2024-06-14 20:35:01 +0100

    lifting the state up/ logic for recognizing the active player and their symbol

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
 import Player from './components/Player'
 import GameBoard from './components/GameBoard'
+import { useState } from 'react'
 
 function App() {
+  const [ activeplayer, setActivePlayer] = useState('X')
+
+  function handleSelectSquare() {
+    setActivePlayer((currentActivePlayer) => currentActivePlayer === 'X' ? 'O' : 'X' )
+  }
+  
   return (
     <main>
       <div id="game-container">
-        <ol id="players">
-          <Player initialName='Player 1' symbol='X' />
-          <Player initialName='Player 2' symbol='O' />
+        <ol id="players" className='highlight-player'>
+          <Player initialName='Player 1' symbol='X' isActive={activeplayer === 'X'} />
+          <Player initialName='Player 2' symbol='O' isActive={activeplayer === 'O'} />
         </ol>
-        <GameBoard />
+        <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activeplayer} />
       </div>
       LOG
     </main>
