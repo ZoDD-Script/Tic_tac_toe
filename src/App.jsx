3f161bcbd024f2180604e11234e3213d3495616a
Chukwu Blessed <blessedchukwu89@gmail.com>
2024-06-15 22:52:14 +0100

    logging game turns/ state sharing across components/ Deriving state from props

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,29 @@ import { useState } from 'react'
 import Log from './components/Log'
 
 function App() {
+  const [ gameTurns, setGameTurns ] = useState([])
   const [ activeplayer, setActivePlayer] = useState('X')
 
-  function handleSelectSquare() {
-    setActivePlayer((currentActivePlayer) => currentActivePlayer === 'X' ? 'O' : 'X' )
+  function handleSelectSquare(rowIndex, colIndex) {
+    setActivePlayer((currentActivePlayer) => currentActivePlayer === 'X' ? 'O' : 'X' );
+
+    setGameTurns((prevTurns) => {
+      let currentPlayer = 'X';
+
+      if(prevTurns.length > 0 && prevTurns[0].player === 'X') {
+        currentPlayer = 'O';
+      }
+
+      const updatedTurns = [
+        { 
+          square: { row: rowIndex, col: colIndex },
+          player: currentPlayer
+        },
+        ...prevTurns,
+      ]
+
+      return updatedTurns;
+    })
   }
   
   return (
@@ -17,9 +36,11 @@ function App() {
           <Player initialName='Player 1' symbol='X' isActive={activeplayer === 'X'} />
           <Player initialName='Player 2' symbol='O' isActive={activeplayer === 'O'} />
         </ol>
-        <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activeplayer} />
+        <GameBoard onSelectSquare={handleSelectSquare} 
+        // activePlayerSymbol={activeplayer} />
+        turns={gameTurns} />
       </div>
-      <Log />
+      <Log logTurns={gameTurns} />
     </main>
   )
 }
--- src/components/Log.jsx
+++ src/components/Log.jsx
@@ -1,5 +1,7 @@
-export default function Log() {
+export default function Log({ logTurns }) {
   return(
-    <></>
+    <ol id="log">
+      {logTurns.map(turn => <li key={`${turn.square.row}${turn.square.col}`}>{turn.player} selected {turn.square.row + 1}, {turn.square.col + 1}</li>)}
+    </ol>
   )
 }
