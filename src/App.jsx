2d290e402a7f5665a2eca19605437c32a7d12526
Chukwu Blessed <blessedchukwu89@gmail.com>
2024-06-17 09:21:08 +0100

    deciding if the matrch is a draw or a win for a player/ The game over screen

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Player from './components/Player'
 import GameBoard from './components/GameBoard'
 import { useState } from 'react'
 import Log from './components/Log'
+import GameOver from './components/GameOver'
 import { WINNING_COMBINATIONS } from './winning-combinations'
 
 const initialGameBoard = [
@@ -34,9 +35,19 @@ function App() {
     gameBoard[row][col] = player 
   }
 
-  for(const combination of WINNING_COMBINATIONS) {
+  let winner = null;
 
+  for(const combination of WINNING_COMBINATIONS) {
+    const firstSquareSymbol = gameBoard[combination[0].row][combination[0].column]
+    const secondSquareSymbol = gameBoard[combination[1].row][combination[1].column]
+    const thirdSquareSymbol = gameBoard[combination[2].row][combination[2].column]
+
+    if(firstSquareSymbol && firstSquareSymbol === secondSquareSymbol && firstSquareSymbol === thirdSquareSymbol) {
+      winner = firstSquareSymbol
+    }
   }
+
+  const hasDraw = gameTurns.length === 9 && !winner;
 
   function handleSelectSquare(rowIndex, colIndex) {
     // setActivePlayer((currentActivePlayer) => currentActivePlayer === 'X' ? 'O' : 'X' );
@@ -62,6 +73,7 @@ function App() {
           <Player initialName='Player 1' symbol='X' isActive={activeplayer === 'X'} />
           <Player initialName='Player 2' symbol='O' isActive={activeplayer === 'O'} />
         </ol>
+        {(winner || hasDraw) && <GameOver winner={winner} />}
         <GameBoard onSelectSquare={handleSelectSquare} 
         // activePlayerSymbol={activeplayer} />
         board={gameBoard} />
--- src/components/Log.jsx
+++ src/components/Log.jsx
@@ -1,7 +1,7 @@
 export default function Log({ logTurns }) {
   return(
     <ol id="log">
-      {logTurns.map(turn => <li key={`${turn.square.row}${turn.square.col}`}>{turn.player} selected {turn.square.row + 1}, {turn.square.col + 1}</li>)}
+      {logTurns.map(turn => <li key={`${turn.square.row}${turn.square.col}`}>{turn.player} selected {turn.square.row}, {turn.square.col}</li>)}
     </ol>
   )
 }
