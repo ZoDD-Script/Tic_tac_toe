bc34fe37fe634bb15e80c0961011344f780a7af6
Chukwu Blessed <blessedchukwu89@gmail.com>
2024-06-17 08:21:13 +0100

    lifting computed values up/ checking for possible combinations

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,12 @@ import { useState } from 'react'
 import Log from './components/Log'
 import { WINNING_COMBINATIONS } from './winning-combinations'
 
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+]
+
 function deriveActivePlayer(gameTurns) {
   let currentPlayer = 'X';
 
@@ -18,6 +24,19 @@ function App() {
   const [ gameTurns, setGameTurns ] = useState([])
   // const [ activeplayer, setActivePlayer] = useState('X')
   const activeplayer = deriveActivePlayer(gameTurns);
+
+  let gameBoard = initialGameBoard;
+
+  for(const turn of gameTurns) {
+    const { square, player } = turn;
+    const { row, col } = square;
+
+    gameBoard[row][col] = player 
+  }
+
+  for(const combination of WINNING_COMBINATIONS) {
+
+  }
 
   function handleSelectSquare(rowIndex, colIndex) {
     // setActivePlayer((currentActivePlayer) => currentActivePlayer === 'X' ? 'O' : 'X' );
@@ -45,7 +64,7 @@ function App() {
         </ol>
         <GameBoard onSelectSquare={handleSelectSquare} 
         // activePlayerSymbol={activeplayer} />
-        turns={gameTurns} />
+        board={gameBoard} />
       </div>
       <Log logTurns={gameTurns} />
     </main>
